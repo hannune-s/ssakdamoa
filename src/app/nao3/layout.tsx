f1fdc38b9b214a123324b5e0ffb7748085504390
hannune-s <wkhn11@gmail.com>
2026-09-03 15:02:42 +0900

    revert: remove nao3 prototype from ssakdamoa project to maintain independence

--- src/app/nao3/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function Nao3Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="min-h-screen bg-gray-50 font-sans pb-24">
-      {children}
-    </div>
-  );
-}
